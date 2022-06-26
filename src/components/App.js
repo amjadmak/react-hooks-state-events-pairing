2494d39dfd01f95ebb651f6e364e91d6463f3e72
Amjad <amjadmaqsuma@gmail.com>
2022-06-26 11:04:11 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,42 @@
 import video from "../data/video.js";
+import Comment from "./Comment.jsx";
+import React, { useState } from "react";
 
 function App() {
-  console.log("Here's your data:", video);
+
+  const [upVote, setUpVote] = useState(video.upvotes);
+  const [downVote, setDownVote] = useState(video.downvotes);
+  const [isHidden, setIsHidden] = useState(false);
+const thumbsUp = () => {
+  setUpVote(upVote + 1);
+}
+const thumbsDown = () => {
+  setDownVote(downVote + 1);
+
+}
 
   return (
     <div className="App">
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={video.embedUrl}
         frameBorder="0"
         allowFullScreen
-        title="Thinking in React"
+        title={video.title}
       />
+    
+    <h2>{video.title}</h2>
+    <span>{video.views} Views | Uploaded {video.createdAt}</span>
+    <div><button onClick={thumbsUp}>👍{upVote}</button><button onClick={thumbsDown}>👎{downVote}</button></div>
+    <div className="hideComments" ><button onClick={() =>{setIsHidden(!isHidden)}}>Hide Comments</button></div>
+    <div><h2>{video.comments.length} Comments</h2></div>
+    {!isHidden ? null : <div>{video.comments.map((comment) =>{
+    
+    return <Comment key={comment.id} comment={comment.comment} user={comment.user}/>
+
+  })}</div>}
+
     </div>
   );
 }
